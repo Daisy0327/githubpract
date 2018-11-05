a1497ed5c2ccb1efd1f5f2dfa640658bcbf6d05b
Daisy Sanchez-373001371 <373001371@vusdnoc.vusd.k12.ca.us>
2018-11-05 11:21:47 -0800

    Initial Commit

---
+++ src/githubpract/Githubpract.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubpract;
+
+/**
+ *
+ * @author 373001371
+ */
+public class Githubpract {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("no");
+    }
+    
+}
